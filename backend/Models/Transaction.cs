3398d53495877d7433b55329d1206bbd5d468d83
marcelo <marceloconti9017@gmail.com>
2025-02-10 15:22:51 -0300

    :sparkles: feat: Create User and Transaction models

---
+++ backend/Models/Transaction.cs
@@ -0,0 +1,8 @@
+public class Transaction
+{
+    public int Id {get; set;}
+    public string Description {get; set;}
+    public decimal Value {get; set;}
+    public string Type {get; set;}
+    public int UserId {get; set;}
+}

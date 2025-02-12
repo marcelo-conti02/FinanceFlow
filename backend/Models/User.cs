ff3bc61f798a0f7e8c95588a58806c3c9b4f7f28
marcelo <marceloconti9017@gmail.com>
2025-02-12 13:54:40 -0300

    :sparkles: feat: Add constructor to User model

--- backend/Models/User.cs
+++ backend/Models/User.cs
@@ -3,4 +3,10 @@ public class User
     public int Id {get; set;}
     public string Name {get; set;}
     public int Age {get; set;}
+
+    public User(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
 }

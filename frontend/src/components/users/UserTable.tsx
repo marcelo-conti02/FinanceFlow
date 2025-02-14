8b9ebd5da8e6617677ee0b457edea28988ce84df
marcelo <marceloconti9017@gmail.com>
2025-02-14 14:08:53 -0300

    :sparkles: feat: Edit UserTableItem and UserTable components

--- frontend/src/components/users/UserTable.tsx
+++ frontend/src/components/users/UserTable.tsx
@@ -1,5 +1,36 @@
-export default function UserTable(){
+import { useEffect, useState } from "react"
+import { UserDto } from "../../models/userDto"
+import userService from "../../services/userService";
+import UserTableItem from "./UserTableItem";
+import "../../styles/App.css"
+
+export default function UserTable() {
+    const [users, setUsers] = useState<UserDto[]>([]);
+
+    useEffect(() => {
+        userService.getUsers().then(setUsers);
+    }, []);
+
+    const listUsers = () => {
+        return users.map((user) => (
+            <UserTableItem key={user.id} name={user.name} age={user.age} />
+        ))
+    };
+
     return (
-        <></>
+        <>
+            <div className="header">
+                <h1>Finance Flow</h1>
+                <div className="buttons">
+                    <button className="button">Register user</button>
+                    <button className="button">Check total</button>
+                </div>
+            </div>
+            <div className="body">
+                <div className="userTable">
+                    {listUsers()}
+                </div>
+            </div>
+        </>
     )
 }
--- frontend/src/components/users/UserTableItem.tsx
+++ frontend/src/components/users/UserTableItem.tsx
@@ -1,5 +1,11 @@
-export default function UserTableItem(){
+import "../../styles/App.css"
+
+export default function UserTableItem(props: { name: string, age: number }) {
     return (
-        <></>
+        <div className="user">
+            <h1>{props.name}</h1>
+            <p>{props.age} years old</p>
+            <button>Register transaction</button>
+        </div>
     )
 }

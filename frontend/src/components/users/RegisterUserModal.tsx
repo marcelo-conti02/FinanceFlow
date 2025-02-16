a68c6e9527a92f2c644ee8f4359c27522497fca0
marcelo <marceloconti9017@gmail.com>
2025-02-15 23:22:10 -0300

    :sparkles: feat: Add some styles to RegisterUserModal

--- frontend/src/components/users/RegisterUserModal.tsx
+++ frontend/src/components/users/RegisterUserModal.tsx
@@ -7,33 +7,38 @@ export default function RegisterUserModal(props: { setIsOpen: (isOpen: boolean) 
     const [age, setAge] = useState("");
 
     const handleSubmit = async (e: React.FormEvent) => {
-        // prevents page refresh
         e.preventDefault();
         try {
-            // uses Partial to register without an Id
-            const user: Partial<UserDto> = { name, age: Number(age)};
+            const user: Partial<UserDto> = { name, age: Number(age) };
             await userService.createUser(user as UserDto);
             props.setIsOpen(false);
+            window.location.reload();
         } catch (error) {
             console.error("Failed to create user:", error);
         }
     };
 
     return (
-        <div>
-            <h2>Register User</h2>
-            <form onSubmit={handleSubmit}>
-                <label>
-                    Name:
-                    <input type="text" name="name" autoComplete="off" value={name} onChange={(e) => setName(e.target.value)} required/>
-                </label>
-                <label>
-                    Age:
-                    <input type="number" name="age" autoComplete="off" value={age} onChange={(e) => setAge(e.target.value)} required/>
-                </label>
-                <button type="submit">Save</button>
-                <button type="button" onClick={() => props.setIsOpen(false)}>Close</button>
-            </form>
+        <div className="modal">
+            <div className="modal-content">
+                <h2>Register User</h2>
+                <form onSubmit={handleSubmit}>
+                    <div className="formUser">
+                        <label>
+                            Name:
+                            <input className="formUserInput" type="text" name="name" autoComplete="off" value={name} onChange={(e) => setName(e.target.value)} required />
+                        </label>
+                        <label>
+                            Age:
+                            <input className="formUserInput" type="number" name="age" autoComplete="off" value={age} onChange={(e) => setAge(e.target.value)} required />
+                        </label>
+                    </div>
+                    <div className="formUserBtns">
+                        <button type="submit">Save</button>
+                        <button type="button" onClick={() => props.setIsOpen(false)}>Close</button>
+                    </div>
+                </form>
+            </div>
         </div>
-    )
+    );
 }
